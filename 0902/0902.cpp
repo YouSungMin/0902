9574532a81e9dfdd69149b638923dbd77aca1106
KGA <KGA@DESKTOP-9RLMNSK>
2025-09-02 11:52:44 +0900

    초기 커밋.

---
+++ 0902/0902.cpp
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include "header.h"
+#include <stdio.h>
+
+int main()
+{
+    /*printf("Hello world\n");
+    printf("유성민 \"");
+    printf("00년생입니다.\"\n");
+    printf("□□□□□□□□□□□□□□□□□□□□□□□□");*/
+
+    int num = 0;
+    scanf("%d", &num);
+    printf("입력된 숫자는(c) : %d\n", num);
+    std::cin >> num;
+    printf("입력된 숫자는(c++) : %d\n", num);
+}
