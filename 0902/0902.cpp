e261d80fa29bead56156d40992826f731d822574
Sungmin <xormraoghk@gmail.com>
2025-09-02 15:34:13 +0900

    실습1~5

--- 0902/0902.cpp
+++ 0902/0902.cpp
@@ -15,9 +15,48 @@ int main()
     printf("입력된 숫자는(c) : %d\n", num);
     std::cin >> num;
     printf("입력된 숫자는(c++) : %d\n", num);*/
+    int num1 = 0, num2 = 0;
+    int tmp;
+    int i = 0, j = 0;
+    /*
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j <= i*2; j++)
+            printf("*");
+        printf("\n");
+    }*/
+    
+    for (i = 0; i < 8; i++) {
+        printf("□");
+    }
+    printf("\n");
+    printf("□  □    □  □");
+    printf("\n");
+    printf("□     □     □");
+    printf("\n");
 
-    int num = 0;
+    for (i = 0; i < 8; i++) {
+        printf("□");
+    }
 
-    printf("123123");//변경2
-    //변경3
+    printf("두 수를 입력해주세요: ");
+    std::cin >> num1;
+    std::cin >> num2;
+    tmp = num1;
+    num1 = num2;
+    num2 = tmp;
+    printf("num1 = %d, num2 = %d", num1, num2);
+
+    printf("가로 길이 입력 : ");
+    std::cin >> num1;
+    printf("세로 길이 입력 : ");
+    std::cin >> num2;
+    printf("사각형의 넓이 : %d",num1*num2);
+    
+    printf("두 수를 입력해주세요: ");
+    std::cin >> num1;
+    std::cin >> num2;
+    tmp = num1;
+    while (tmp >= num2)
+        tmp -= num2;
+    printf("%d를 %d로 나눈 나머지는 %d 입니다", num1, num2, tmp);
 }
